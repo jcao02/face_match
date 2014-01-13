5d28756064f7b5f72658c6eada5765f53e47e709
Juan Carlos Arocha Ovalles <juan@chronos>
2014-01-13 22:04:33 +0100

    No image extension validation

--- face_match/forms.py
+++ face_match/forms.py
@@ -5,12 +5,12 @@ import os.path
 class ImageDetected(forms.Form):
     photo = forms.ImageField()
 
-    def is_valid(self):
-        """Validates image extension"""
-        valid = super(ImageDetected, self).is_valid()
-        ext = self.get_extension()
-        valid = valid and (ext == ".jpeg" or ext == ".JPEG")
-        return valid
+#    def is_valid(self):
+        #"""Validates image extension"""
+        #valid = super(ImageDetected, self).is_valid()
+        #ext = self.get_extension()
+        #valid = valid and (ext == ".jpeg" or ext == ".JPEG")
+#        return valid
 
     def get_extension(self):
         """Gets file extension"""
